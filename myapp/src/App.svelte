c823cc54c1acb7529c9538f4fdd842bb312269a9
Galaad-LSV <g.lusven.ledantec@gmail.com>
2022-10-07 11:22:15 +0200

    ajout pokemonapi

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,45 +1,53 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  let limit = 20;
+  let page = 1;
+  let offset = 0;
+  $: {offset = (page - 1) * limit;}
+
+  async function getPokedexes() {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+    const data = await response.json();
+    pokemonList = data.results;
+  }
+  
+  let pokemonList = [];
+
 </script>
 
 <main>
+  <h1>Acces à l'API Pokemon</h1>
+
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <label for="limit">
+      Nombre de pokemons à afficher
+      <input id="limit" name="limit" type="number" min="5" max="20" bind:value={limit}>
+    </label>
+
+    <label for="offset">
+      Page
+      <input id="offset" name="offset" type="number" min="1" max="50" bind:value={page}>
+    </label>
+
+    <button id="submit" type="button" on:click={getPokedexes}>Ok</button>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  {#await getPokedexes()}
+    <p>Chargement...</p>
+  {:then}
+    <ul>
+      {#each pokemonList as pokemon (pokemon.name)}
+        <li>{pokemon.name}</li>
+      {/each}
+    </ul>
+  {:catch error}
+    <p>Une erreur est survenue</p>
+    <p>{error}</p>
+  {/await}
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  li {
+    list-style: none;
   }
 </style>
